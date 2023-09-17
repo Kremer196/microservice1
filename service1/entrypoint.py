f09537971d484a26c3980ae632cc7cc69da281a5
Marko <marko.kremer@fer.hr>
2023-09-18 01:06:06 +0200

    modified service1 code

--- service1/entrypoint.py
+++ service1/entrypoint.py
@@ -2,13 +2,16 @@ import sys
 import hashlib
 
 inp = sys.stdin.readlines()
-hash_func = inp[0].strip()
-message = '\n'.join(inp[1:]).strip()
+if inp:
+    hash_func = inp[0].strip()
+    message = '\n'.join(inp[1:]).strip()
 
-if hash_func in hashlib.algorithms_available:
-    h = hashlib.new(hash_func)
-    h.update(str.encode(message))
+    if hash_func in hashlib.algorithms_available:
+        h = hashlib.new(hash_func)
+        h.update(str.encode(message))
 
-    print(h.hexdigest())
+        print(h.hexdigest())
+    else:
+        print("Enter valid hash function, valid hash functions are: ", hashlib.algorithms_available)
 else:
-    print("Enter valid hash function, valid hash functions are: ", hashlib.algorithms_available)
+    print("Please provide a hash function and what you want to hash")
